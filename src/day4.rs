73709c148a7b8e669d79c8124e90d684a891ea47
Daniel Sharp <dannys4@mit.edu>
2024-12-05 08:33:13 -0500

    Finish day 4

--- src/day4.rs
+++ src/day4.rs
@@ -102,25 +102,85 @@ fn part1(word_search: &String, word: &String) -> i32 {
     return count;
 }
 
-fn create_3x3(array: &String) -> Vec<String> {
+// Class that represents the X in the 3x3 tile
+struct XTile {
+    x: (char, char),
+    y: (char, char),
+    z: char
+}
+
+fn print_rows(row0: &[char], row1: &[char], row2: &[char]) {
+    println!("{:?}", row0);
+    println!("{:?}", row1);
+    println!("{:?}", row2);
+    println!();
+}
+
+fn print_xtile(tile: &XTile) {
+    println!("{}.{}", tile.x.0, tile.y.0);
+    println!(".{}.", tile.z);
+    println!("{}.{}", tile.y.1, tile.x.1);
+    println!();
+}
+
+impl XTile {
+    fn new(row0: &[char], row1: &[char], row2: &[char]) -> XTile {
+        let x = (row0[0], row2[2]);
+        let y = (row0[2], row2[0]);
+        let z = row1[1];
+        XTile { x, y, z }
+    }
+}
+
+fn is_xmas(tile: XTile) -> bool {
+    let XTile { x, y, z } = tile;
+    if z != 'A' {
+        // println!("Invalid z: {}", z);
+        return false;
+    }
+    if (x.0 != 'M' || x.1 != 'S') && (x.0 != 'S' || x.1 != 'M') {
+        // println!("Invalid x diag: {}{}{}", x.0, z, x.1);
+        return false;
+    }
+    if (y.0 != 'M' || y.1 != 'S') && (y.0 != 'S' || y.1 != 'M') {
+        // println!("Invalid y diag: {}{}{}", y.0, z, y.1);
+        return false;
+    }
+    return true;
+}
+
+fn create_3x3(array: &String) -> Vec<XTile> {
     let (row_len, rows) = create_rows(array);
-    let mut three_by_three: Vec<String> = Vec::new();
+    let mut three_by_three: Vec<XTile> = Vec::new();
     for i in 0..rows.len()-2 {
         for j in 0..row_len-2 {
-            let mut s = String::new();
-            for k in 0..3 {
-                for l in 0..3 {
-                    s.push(rows[i+k][j+l]);
-                }
-            }
-            three_by_three.push(s);
+            let row0 = &rows[i][j..j+3];
+            let row1 = &rows[i+1][j..j+3];
+            let row2 = &rows[i+2][j..j+3];
+            // print_rows(row0, row1, row2);
+            let tile = XTile::new(row0, row1, row2);
+            // print_xtile(&tile);
+            three_by_three.push(tile);
         }
     }
     return three_by_three;
 }
 
+fn part2(word_search: &String) -> i32 {
+    let three_by_three = create_3x3(word_search);
+    let mut count = 0;
+    for tile in three_by_three {
+        if is_xmas(tile) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
 pub fn fcn(word_search: &String) {
     let word = "XMAS".to_string();
-    let result = part1(word_search, &word);
-    println!("Part 1: {}", result);
+    let p1 = part1(word_search, &word);
+    println!("Part 1: {}", p1);
+    let p2 = part2(word_search);
+    println!("Part 2: {}", p2);
 }
