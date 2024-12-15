b4bed63a5f4c1d18bfe1d5bc273c065c9e62ef2c
Daniel Sharp <dannys4@mit.edu>
2024-12-15 10:22:54 -0500

    Give up on day12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -32,7 +32,7 @@ fn check_next_pos(pos: (usize, usize), dir: Direction, maxes: (usize, usize)) ->
 }
 
 // Return how much to add to perimeter
-fn explore_direction(grid: &Vec<Vec<char>>, visited: &Vec<Vec<bool>>, c: char, pos: (usize, usize), dir: Direction, stacks: &mut HashMap<char, Vec<(usize, usize)>>) -> usize {
+fn explore_direction_old(grid: &Vec<Vec<char>>, visited: &Vec<Vec<bool>>, c: char, pos: (usize, usize), dir: Direction, stacks: &mut HashMap<char, Vec<(usize, usize)>>) -> usize {
     let maxes = (grid.len() - 1, grid[pos.0].len()-1);
     let next_pos_opt = check_next_pos(pos, dir, maxes);
     if next_pos_opt.is_none() {
@@ -50,14 +50,14 @@ fn explore_direction(grid: &Vec<Vec<char>>, visited: &Vec<Vec<bool>>, c: char, p
     return p_ret
 }
 
-fn explore_tile(grid: &Vec<Vec<char>>, visited: &Vec<Vec<bool>>, c: char, pos: (usize, usize), stacks: &mut HashMap<char, Vec<(usize, usize)>>) -> usize {
+fn explore_tile_old(grid: &Vec<Vec<char>>, visited: &Vec<Vec<bool>>, c: char, pos: (usize, usize), stacks: &mut HashMap<char, Vec<(usize, usize)>>) -> usize {
     // Explore in all four directions (or as many as possible).
     // In every direction that has a different character, add one to the perimeter
     // print!("{}: ({},{})", c, pos.0, pos.1);
-    let east_p = explore_direction(grid, visited, c, pos, Direction::East, stacks);
-    let west_p = explore_direction(grid, visited, c, pos, Direction::West, stacks);
-    let north_p = explore_direction(grid, visited, c, pos, Direction::North, stacks);
-    let south_p = explore_direction(grid, visited, c, pos, Direction::South, stacks);
+    let east_p = explore_direction_old(grid, visited, c, pos, Direction::East, stacks);
+    let west_p = explore_direction_old(grid, visited, c, pos, Direction::West, stacks);
+    let north_p = explore_direction_old(grid, visited, c, pos, Direction::North, stacks);
+    let south_p = explore_direction_old(grid, visited, c, pos, Direction::South, stacks);
     // println!("E{}, W{}, N{}, S{}", c, pos.0, pos.1, east_p, west_p, north_p, south_p);
     // print!("\n");
     return east_p + west_p + north_p + south_p;
@@ -75,7 +75,8 @@ fn find_first_stack(stacks: &HashMap<char, Vec<(usize, usize)>>) -> char {
     return which_stack
 }
 
-fn part1(grid: &Vec<Vec<char>>, unique_chars: &Vec<char>) -> usize {
+
+fn part1_old(grid: &Vec<Vec<char>>, unique_chars: &Vec<char>) -> usize {
     let mut visited: Vec<Vec<bool>> = grid.iter().map(|x| x.iter().map(|_| false).collect::<Vec<bool>>()).collect();
     let mut stacks: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
     let mut area_perims: HashMap<char, (usize, usize)> = HashMap::new();
@@ -94,7 +95,7 @@ fn part1(grid: &Vec<Vec<char>>, unique_chars: &Vec<char>) -> usize {
             let next_pos = stack.pop().unwrap();
             if !visited[next_pos.0][next_pos.1] {
                 let area_perim = area_perims.get_mut(&which_stack).unwrap();
-                let add_perim = explore_tile(grid, &visited, which_stack, next_pos, &mut stacks);
+                let add_perim = explore_tile_old(grid, &visited, which_stack, next_pos, &mut stacks);
                 area_perim.0 += 1;
                 area_perim.1 += add_perim;
                 visited[next_pos.0][next_pos.1] = true;
@@ -106,6 +107,73 @@ fn part1(grid: &Vec<Vec<char>>, unique_chars: &Vec<char>) -> usize {
         println!("{}: {:?}", k, v);
     }
     return ret;
+}
+
+fn find_first_unvisited(visited: &Vec<Vec<bool>>) -> Option<(usize, usize)> {
+    let (n_i,n_j) = (visited.len(), visited[0].len());
+    for i in 0..n_i {
+        for j in 0..n_j {
+            if !visited[i][j] {
+                return Some((i,j));
+            }
+        }
+    }
+    return None;
+}
+
+// Return how much to add to perimeter
+fn explore_direction(grid: &Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>, c: char, pos: (usize, usize), dir: Direction, area:  &mut usize, perim: &mut usize) {
+    let maxes = (grid.len() - 1, grid[pos.0].len()-1);
+    let next_pos_opt = check_next_pos(pos, dir, maxes);
+    if next_pos_opt.is_none() {
+        *perim += 1;
+        return;
+    }
+    let next_pos = next_pos_opt.unwrap();
+    let c_next = grid[next_pos.0][next_pos.1];
+    if c_next == c {
+        visit_helper(grid, visited, next_pos, area, perim);
+    } else {
+        *perim += 1;
+        return
+    }
+}
+
+fn explore_tile(grid: &Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>, c: char, pos: (usize, usize), area: &mut usize, perim: &mut usize) {
+    // Explore in all four directions (or as many as possible).
+    // In every direction that has a different character, add one to the perimeter
+    *area += 1;
+    explore_direction(grid, visited, c, pos, Direction::East, area, perim);
+    explore_direction(grid, visited, c, pos, Direction::West, area, perim);
+    explore_direction(grid, visited, c, pos, Direction::North, area, perim);
+    explore_direction(grid, visited, c, pos, Direction::South, area, perim);
+}
+
+fn visit_helper(grid: &Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>, pos: (usize, usize), area: &mut usize, perim: &mut usize) {
+    if visited[pos.0][pos.1] {
+        return;
+    }
+    let c = grid[pos.0][pos.1];
+    visited[pos.0][pos.1] = true;
+    explore_tile(grid, visited, c, pos, area, perim);
+}
+
+fn part1(grid: &Vec<Vec<char>>, _unique: &Vec<char>) -> usize {
+    let mut visited: Vec<Vec<bool>> = grid.iter().map(|x| x.iter().map(|_| false).collect::<Vec<bool>>()).collect();
+    let mut cost = 0;
+    loop {
+        let first_unvisited = find_first_unvisited(&visited);
+        if first_unvisited.is_none() {
+            break;
+        }
+        let pos = first_unvisited.unwrap();
+        let mut area = 0;
+        let mut perim = 0;
+        visit_helper(grid, &mut visited, pos, &mut area, &mut perim);
+        // println!("Char {}, Start ({},{}), Area: {}, Perimeter: {}", grid[pos.0][pos.1], pos.0, pos.1, area, perim);
+        cost += area * perim;
+    }
+    return cost;
 }
 
 pub fn fcn(contents: &String) {
