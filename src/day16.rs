aa67395806a37384c0a319007b2123d3ea2a36a6
Daniel Sharp <dannys4@mit.edu>
2024-12-26 20:30:40 -0500

    Not working day16

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,4 @@
-use std::{cmp::min, collections::HashSet};
+use std::cmp::min;
 
 const TURN_COST: usize = 1000;
 const STRAIGHT_COST: usize = 1;
@@ -64,17 +64,14 @@ fn is_valid_step(maze: &Maze, curr_pos: (usize, usize), dir: Direction) -> ((usi
     let offset = get_offset(dir);
     let next_pos_opt = get_next_pos(curr_pos, offset);
     if next_pos_opt.is_none() {
-        // println!("\tNext Position is Out-of-bounds");
         return ((0,0), false);
     }
     let next_pos = next_pos_opt.unwrap();
     if next_pos.0 >= maze.maze.len() || next_pos.1 >= maze.maze[next_pos.0].len() {
-        // println!("\tNext Position {:?} is Out-of-bounds", next_pos);
         return ((0,0), false);
     }
     let is_wall = maze.maze[next_pos.0][next_pos.1];
     if is_wall {
-        // println!("\tCurr pos {:?}, Next position {:?} is a wall", curr_pos, next_pos);
         return ((0,0), false);
     }
     return (next_pos, true);
@@ -90,50 +87,69 @@ fn opp_direction(dir: Direction) -> Direction {
 }
 
 // Find quickest way through maze
-fn maze_step(maze: &Maze, curr_pos: (usize, usize), dir: Direction, history:HashSet<(usize,usize)>, curr_cost: usize, curr_best: usize) -> usize {
-    // println!("Current pos: {:?}, direction: {:?}, current_cost: {:?}", curr_pos, dir, curr_cost);
-    if curr_pos == maze.end || curr_cost >= curr_best {
-        // println!("Ending!");
-        return curr_cost;
-    }
-    let mut min_cost = curr_best;
-    let opp_dir = opp_direction(dir);
-    for new_dir in [Direction::ColLf, Direction::ColRt, Direction::RowDn, Direction::RowUp] {
-        if new_dir == opp_dir {continue}
-        // println!("Checking direction {:?}...", new_dir);
-        let (new_pos, is_valid) = is_valid_step(maze, curr_pos, new_dir);
-        // if is_valid {println!("\tValid direction!")};
-        if is_valid && !history.contains(&new_pos) {
-            let mut new_history = history.clone();
-            new_history.insert(new_pos);
-            let new_add_cost = STRAIGHT_COST + ((dir != new_dir) as usize)*TURN_COST;
-            let new_total_cost = maze_step(maze, new_pos, new_dir, new_history, curr_cost + new_add_cost, min_cost);
-            min_cost = min(min_cost, new_total_cost);
+fn solve_maze(maze: &Maze) -> (usize,Vec<(usize,usize)>) {
+    // Treat the graph as (position, direction, cost) tuple
+    let mut next_visit: Vec<((usize,usize),Direction,usize,Vec<(usize,usize)>)> = Vec::new();
+    let mut visited: Vec<Vec<bool>> = vec![vec![false; maze.maze[0].len()]; maze.maze.len()];
+    next_visit.push((maze.start, START_DIR, 0, Vec::new()));
+    let mut min_cost = usize::MAX;
+    let mut min_cost_history: Option<Vec<(usize,usize)>> = None;
+    loop {
+        let curr_pos_opt = next_visit.pop();
+        if curr_pos_opt.is_none() {
+            break;
+        }
+        let (curr_pos, curr_dir, curr_cost, history) = curr_pos_opt.unwrap();
+        if curr_pos == maze.end {
+            if curr_cost < min_cost {
+                min_cost = curr_cost;
+                min_cost_history = Some(history);
+            }
+            continue;
+        }
+        if visited[curr_pos.0][curr_pos.1] {
+            continue;
+        }
+        visited[curr_pos.0][curr_pos.1] = true;
+        let mut history_copy = history.clone();
+        history_copy.push(curr_pos);
+        for next_dir in vec![Direction::ColLf, Direction::ColRt, Direction::RowUp, Direction::RowDn] {
+            let (next_pos, is_valid) = is_valid_step(maze, curr_pos, next_dir);
+            if !is_valid {
+                continue;
+            }
+            let next_cost = if next_dir == curr_dir {STRAIGHT_COST} else if next_dir == opp_direction(curr_dir) {STRAIGHT_COST+2*TURN_COST} else {STRAIGHT_COST+TURN_COST};
+            next_visit.push((next_pos, next_dir, curr_cost + next_cost, history_copy.clone()));
         }
     }
-    return min_cost;
+    return (min_cost, min_cost_history.unwrap());
+}
+
+fn print_maze_history(maze: &Maze, history: &Vec<(usize,usize)>) {
+    let mut maze_chars: Vec<Vec<char>> = Vec::new();
+    for row in 0..maze.maze.len() {
+        let mut maze_row = Vec::new();
+        for col in 0..maze.maze[row].len() {
+            maze_row.push(if maze.maze[row][col] {'#'} else {'.'});
+        }
+        maze_chars.push(maze_row);
+    }
+    for pos in history {
+        maze_chars[pos.0][pos.1] = 'X';
+    }
+    for row in maze_chars {
+        println!("{}", row.iter().collect::<String>());
+    }
 }
 
 fn part1(maze: &Maze) -> usize {
-    let history: HashSet<(usize,usize)> = HashSet::new();
-    let total_cost = maze_step(maze, maze.start, START_DIR, history, 0, usize::MAX);
-    return total_cost;
+    let (maze_cost, maze_history) = solve_maze(maze);
+    print_maze_history(maze, &maze_history);
+    return maze_cost;
 }
-
-// fn print_maze(maze: &Maze, pos: (usize, usize)) {
-//     for (i,line) in maze.maze.iter().enumerate() {
-//         for (j, is_wall) in line.iter().enumerate() {
-//             let is_pos = (i,j) == pos;
-//             let print_str = if is_pos {'X'} else { if *is_wall {'#'} else {'.'}};
-//             print!("{}",print_str);
-//         }
-//         println!();
-//     }
-// }
 
 pub fn fcn(contents: &String) {
     let maze = parse_contents(contents);
-    // print_maze(&maze, maze.start);
     let p1 = part1(&maze);
     println!("Part 1: {p1}");
 }
